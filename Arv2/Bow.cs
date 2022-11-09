e9b3c90c39ec204272811887f4e593751bb8f9d5
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2022-11-09 12:38:59 +0100

    hbjsd

---
+++ Arv2/Bow.cs
@@ -0,0 +1,10 @@
+
+
+public class Bow : Rangeweapon
+{
+    public Bow()
+    {
+        Damage = 5;
+        Range = 3;
+    }
+}
--- Arv2/Rangeweapon.cs
+++ Arv2/Rangeweapon.cs
@@ -4,4 +4,11 @@ public class Rangeweapon : weapon
 {
     public int Range { get; set; } = 10;
 
+    public Rangeweapon()
+    {
+        Damage = 20;
+
+    }
 }
+
+
